2300d742c84e11a8b763f4f7d0210952d73efcee
greatshah222 <great.shah222@gmail.com>
2018-05-03 23:00:16 +0300

    added new controller and basic layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,288 @@
+
+<!DOCTYPE HTML>
+<html lang="zxx">
+<head>
+    <title>Handout a Society Category Bootstrap Responsive Web Template | Home :: w3layouts</title>
+    <!-- Meta tag Keywords -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Handout Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--// Meta tag Keywords -->
+    <!-- css files -->
+    <link href="{{asset('dist/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" /> <!-- Bootstrap-Core-CSS -->
+    <link href="{{asset('dist/css/style.css')}}" rel="stylesheet" type="text/css" media="all" /> <!-- Style-CSS -->
+    <link rel="stylesheet" href="{{asset('dist/css/font-awesome.css')}}"> <!-- Font-Awesome-Icons-CSS -->
+    <link href="{{asset('dist/css/pogo-slider.min.css')}}" rel="stylesheet" type="text/css" media="all" /> <!-- Banner-Slider-CSS -->
+    <link href="{{asset('dist/css/nav.css')}}" rel="stylesheet" type="text/css" media="all" /> <!-- Nav-CSS -->
+    <link rel="stylesheet" href="{{asset('dist/css/owl.carousel.css')}}" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
+    <link href="{{asset('dist/css/lsb.css')}}" rel="stylesheet" type="text/css"> <!-- gallery-css -->
+    <!-- //css files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Lobster&amp;subset=cyrillic,latin-ext,vietnamese" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Dosis:300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
+    <!-- //online-fonts -->
+    <!-- js files -->
+    <script src="{{asset('dist/s/modernizr.js')}}j"></script>
+    <!-- /js files -->
+</head>
+<body>
+
+<!-- banner section -->
+<section class="banner-w3ls">
+    <div class="banner-w3l-mk">
+        <div class="logo-w3ls">
+            <a href="#"><h1><i class="fa fa-gratipay" aria-hidden="true"></i>Handout</h1></a>
+        </div>
+        <div class="w3_agileits_header_text">
+            <h3>Donate<i class="fa fa-bank" aria-hidden="true"></i></h3>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="navigation-w3ls">
+        <button id="trigger-overlay" type="button"><i class="fa fa-bars" aria-hidden="true"></i></button>
+        <div class="overlay overlay-door">
+            <button type="button" class="overlay-close"><i class="fa fa-times" aria-hidden="true"></i></button>
+            <nav>
+                <ul>
+                    <li><a href="index.html" class="scroll">Home</a></li>
+                    <li><a href="#services" class="scroll">Services</a></li>
+                    <li><a href="#features" class="scroll">Features</a></li>
+                    <li><a href="#about" class="scroll">About Us</a></li>
+
+                    <li><a href="#gallery" class="scroll">Gallery</a></li>
+                    <li><a href="#team" class="scroll">Volunteers</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="pogoSlider" id="js-main-slider">
+        <div class="pogoSlider-slide" data-transition="verticalSlide" data-duration="2000"  style="background-image:url(images/banner1.jpg);">
+            <div class="pogoSlider-slide-element">
+                <div class="container">
+                    <h3>They are Humans as Well</h3>
+                    <p>Lorem ipsum dolor sit amet, adipiscing elit.</p>
+                    <ul class="banner-agileits">
+                        <li><a href="#readmore" class="link-w3l1" data-toggle="modal">Read More</a></li>
+                        <li><a href="#mail" class="link-w3l2 scroll">Contact Us</a></li>
+                    </ul>
+                    <a href="#about" class="btn btn-circle scroll">
+                        <i class="fa fa-hand-o-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="pogoSlider-slide " data-transition="verticalSlide" data-duration="2000"  style="background-image:url(images/banner2.jpg);">
+            <div class="pogoSlider-slide-element">
+                <div class="container">
+                    <h3>Together We Can Stop poverty!</h3>
+                    <p>Lorem ipsum dolor sit amet, adipiscing elit.</p>
+                    <ul class="banner-agileits">
+                        <li><a href="#readmore" class="link-w3l1" data-toggle="modal">Read More</a></li>
+                        <li><a href="#mail" class="link-w3l2 scroll">Contact Us</a></li>
+                    </ul>
+                    <a href="#about" class="btn btn-circle scroll">
+                        <i class="fa fa-hand-o-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="pogoSlider-slide " data-transition="verticalSlide" data-duration="2000"  style="background-image:url(images/banner3.jpg);">
+            <div class="pogoSlider-slide-element">
+                <div class="container">
+                    <h3>We Can Make Smile!</h3>
+                    <p>Lorem ipsum dolor sit amet, adipiscing elit.</p>
+                    <ul class="banner-agileits">
+                        <li><a href="#readmore" class="link-w3l1" data-toggle="modal">Read More</a></li>
+                        <li><a href="#mail" class="link-w3l2 scroll">Contact Us</a></li>
+                    </ul>
+                    <a href="#about" class="btn btn-circle scroll">
+                        <i class="fa fa-hand-o-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="pogoSlider-slide " data-transition="verticalSlide" data-duration="2000"  style="background-image:url(images/banner4.jpg);">
+            <div class="pogoSlider-slide-element">
+                <div class="container">
+                    <h3>Education For Every Kid!</h3>
+                    <p>Lorem ipsum dolor sit amet, adipiscing elit.</p>
+                    <ul class="banner-agileits">
+                        <li><a href="#readmore" class="link-w3l1" data-toggle="modal">Read More</a></li>
+                        <li><a href="#mail" class="link-w3l2 scroll">Contact Us</a></li>
+                    </ul>
+                    <a href="#about" class="btn btn-circle scroll">
+                        <i class="fa fa-hand-o-down" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div><!-- //pogoSlider -->
+
+    <div class="modal fade" id="readmore" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Handout</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <img class="img-responsive team-agile" src="images/model.jpg" alt="">
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas elementum interdum aliquet. Nam eleifend ac augue in aliquet. Maecenas ac molestie mi. Nam tincidunt pretium dolor, eu placerat elit. Duis nec luctus dolor. Ut a diam metus. Aenean fringilla purus enim, non pharetra eros blandit in. Suspendisse tellus quam, vestibulum vel urna vitae, sagittis consectetur dui. </p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div><!-- //modal-content -->
+        </div><!-- //modal-dialog -->
+    </div><!-- //modal -->
+    </section>
+
+
+
+@yield('value')
+
+
+
+
+
+    <div class="footer">
+	<div class="container">
+		<!-- Social-icons -->
+    <div class="icons-w3l">
+        <ul>
+            <li><a class="facebook" href="#"><i class="fa fa-facebook" aria-hidden="true"></i><span>Facebook</span></a></li>
+            <li><a class="twitter" href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a></li>
+            <li><a class="googleplus" href="#"><i class="fa fa-google-plus" aria-hidden="true"></i><span>Google+</span></a></li>
+            <li><a class="dribbble" href="#"><i class="fa fa-dribbble" aria-hidden="true"></i><span>Dribbble</span></a></li>
+        </ul>
+    </div>
+    <!-- //Social-icons -->
+    <div class="logo-footer">
+        <div class="col-md-3 agileinfo_footer_grid">
+            <h4>About Us</h4>
+            <p>Duis aute irure dolor in esse cillum dolore eu
+                fugiat nulla pariatur. <span>Excepteur sint proident
+					sunt in culpa qui officia anim id est laborum.</span></p>
+            <p class="adam">- Mark Thomson, CEO</p>
+        </div>
+        <div class="col-md-3 agileinfo_footer_grid f1">
+            <h4>Instagram Posts</h4>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo1.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo2.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo3.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo4.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo5.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="agileinfo_footer_grid1">
+                <a href="#"><img src="images/fo6.jpg" alt=" " class="img-responsive"></a>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <div class="col-md-3 agileinfo_footer_grid f3">
+            <h4>Navigation</h4>
+            <div class="nav-w3-l">
+                <ul>
+                    <li><a href="index.html" >Home</a></li>
+                    <li><a href="#about" class="scroll" >About Us</a></li>
+                    <li><a href="#services" class="scroll" >Services</a></li>
+                    <li><a href="#features" class="scroll" >Features</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3 agileinfo_footer_grid f3 last-w3l-nav">
+            <div class="nav-w3-l">
+                <ul>
+                    <li><a href="#team" class="scroll" >Volunteers</a></li>
+                    <li><a href="#gallery" class="scroll" >Gallery</a></li>
+                    <li><a href="#mail" class="scroll" >Mail Us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <!-- copyright -->
+    <div class="w3agile_footer_copy">
+        <p>jfn</p>
+    </div>
+    <!-- //copyright -->
+    <!-- newsletter -->
+
+    <div class="clearfix"> </div>
+    <!-- //newsletter -->
+    </div>
+    </div>
+    </div>
+    <!-- //footer -->
+
+    <!-- js files -->
+    <!-- js -->
+    <script type="text/javascript" src="{{asset('dist/js/jquery-2.1.4.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('dist/js/bootstrap.js')}}"></script> <!-- Necessary-JavaScript-File-For-Bootstrap -->
+    <!-- //js -->
+    <!-- js for banner -->
+    <script src="{{asset('dist/js/jquery.pogo-slider.min.js')}}"></script>
+    <script src="{{asset('dist/js/main.js')}}"></script>
+    <!-- /js for banner -->
+    <!-- js for navigation -->
+    <script src="{{asset('dist/js/classie.js')}}"></script>
+    <script src="{{asset('dist/js/demo1.js')}}"></script>
+    <!-- /js for navigation -->
+    <!-- Gallery -->
+    <script src="{{asset('dist/js/lsb.min.js')}}"></script>
+    <script>
+        $(window).load(function() {
+            $.fn.lightspeedBox();
+        });
+    </script>
+    <!-- //Gallery -->
+    <!-- Numscroller -->
+    <script type="text/javascript" src="{{asset('dist/js/numscroller-1.0.js')}}"></script>
+    <!-- //Numscroller -->
+    <!-- Owl-Carousel-JavaScript -->
+    <script src="{{asset('dist/js/owl.carousel.js')}}"></script>
+    <script>
+        $(document).ready(function() {
+            $("#owl-demo").owlCarousel ({
+                items : 3,
+                lazyLoad : true,
+                autoPlay : true,
+                pagination : true,
+            });
+        });
+    </script>
+    <!-- //Owl-Carousel-JavaScript -->
+    <!-- start-smoth-scrolling -->
+    <script type="text/javascript" src="{{asset('dist/js/move-top.js')}}"></script>
+    <script type="text/javascript" src="{{asset('dist/js/easing.js')}}"></script>
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+            });
+        });
+    </script>
+    <!-- start-smoth-scrolling -->
+    <!-- smooth scrolling -->
+    <script src="{{asset('dist/js/SmoothScroll.min.js')}}"></script>
+    <!-- //smooth scrolling -->
+
+    <!-- //js files -->
+</body>
+</html>
